3dc53fe5e6b8707ba5050c36dd2992c8bbbd0215
Johannes Steudle <johannes.steudle@bosch-softtec.com>
2016-05-08 13:40:59 +0200

    Initial commit

---
+++ OpenHome/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  OpenHome
+//
+//  Created by Johannes on 16.02.16.
+//  Copyright (c) 2016 Johannes Steudle. All rights reserved.
+//
+
+
+import UIKit
+
+
+class FirstViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
