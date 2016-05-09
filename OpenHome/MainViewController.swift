0333eed7592b8cb1dab1c442fb75d7deb0ceacb1
Johannes Steudle <johannes.steudle@bosch-softtec.com>
2016-05-09 14:16:21 +0200

    Add travis ci file

--- OpenHome/MainViewController.swift
+++ OpenHome/MainViewController.swift
@@ -6,114 +6,111 @@
 //  Copyright (c) 2016 Johannes Steudle. All rights reserved.
 //
 
-
 import UIKit
 import HomeKit
 
-
 class MainViewController: UITableViewController, HMHomeManagerDelegate {
-	
-	let homeManager = HMHomeManager()
-	var activeHome: HMHome?
-	
-    override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-		print("viewDidLoad")
-		
-		homeManager.delegate = self
-    }
 
+    let homeManager = HMHomeManager()
+    var activeHome: HMHome?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoad")
+
+        homeManager.delegate = self
+    }
 
     override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
-	// MARK: - Table Delegate
+    // MARK: - Table Delegate
 
-	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		let homes = homeManager.homes
-		print("Found \(homes.count) homes")
-		return homes.count
-	}
-	
-	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCellWithIdentifier("deviceId") as UITableViewCell?
-		let home = homeManager.homes[indexPath.row] as HMHome
-		cell?.textLabel?.text = home.name
-		
-		// ignore the information service
-		cell?.detailTextLabel?.text = "\(home.accessories.count) service(s)"
-		
-		return (cell != nil) ? cell! : UITableViewCell()
-	}
-	
-	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-		// lastSelectedIndexRow = indexPath.row
-	}
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let homes = homeManager.homes
+        print("Found \(homes.count) homes")
+        return homes.count
+    }
 
-	// MARK: - HomeManager Delegate
-	
-	func homeManagerDidUpdateHomes(manager: HMHomeManager) {
-		print("homeManagerDidUpdateHomes")
-		checkHomeSetup()
-	}
-	
-	// MARK: HomeKit Helper Functions
-	
-	func checkHomeSetup() {
-		if homeManager.primaryHome != nil {
-			self.activeHome = homeManager.primaryHome
-			print("Found primary home: \(activeHome)")
-			title = activeHome!.name
-         self.tableView.reloadData()
-		} else if homeManager.homes.first != nil {
-			self.activeHome = homeManager.homes.first
-			print("Found first home: \(activeHome), set it to primary")
-			self.homeManager.updatePrimaryHome(activeHome!, completionHandler: { (error) in
-				if error != nil {
-					print("Something went wrong when attempting to make this home our primary home. \(error?.localizedDescription)")
-				}
-			})
-         self.tableView.reloadData()
-		} else {
-			print("Found no home")
-			
-			var alertViewTextField = UITextField()
-			var text = "Default"
-			let alertView = UIAlertController(title: "No home found!", message: "Didn't find any home, please create a new one:", preferredStyle: UIAlertControllerStyle.Alert)
-			
-			alertView.addTextFieldWithConfigurationHandler( {(textField) -> Void in
-				alertViewTextField = textField
-			})
-			
-			alertView.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: {(alertAction) -> Void in
-				// update the text
-				text = alertViewTextField.text!
-				
-				self.homeManager.addHomeWithName(text, completionHandler: { (home, error) in
-					// what to do here?
-					if error != nil {
-						print("Something went wrong when attempting to create our home. \(error?.localizedDescription)")
-					} else {
-						print("Successfully added home \(home)")
-						self.activeHome = home
-						self.homeManager.updatePrimaryHome(home!, completionHandler: { (error) in
-							if error != nil {
-								print("Something went wrong when attempting to make this home our primary home. \(error?.localizedDescription)")
-							}
-						})
-                  self.tableView.reloadData()
-					}
-				})
-			}))
-			
-			alertView.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
-			
-			dispatch_async(dispatch_get_main_queue(), {
-				self.presentViewController(alertView, animated: true, completion: nil)
-			})
-		}
-	}
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("deviceId") as UITableViewCell?
+        let home = homeManager.homes[indexPath.row] as HMHome
+        cell?.textLabel?.text = home.name
+
+        // ignore the information service
+        cell?.detailTextLabel?.text = "\(home.accessories.count) service(s)"
+
+        return (cell != nil) ? cell! : UITableViewCell()
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // lastSelectedIndexRow = indexPath.row
+    }
+
+    // MARK: - HomeManager Delegate
+
+    func homeManagerDidUpdateHomes(manager: HMHomeManager) {
+        print("homeManagerDidUpdateHomes")
+        checkHomeSetup()
+    }
+
+    // MARK: HomeKit Helper Functions
+
+    func checkHomeSetup() {
+        if homeManager.primaryHome != nil {
+            self.activeHome = homeManager.primaryHome
+            print("Found primary home: \(activeHome)")
+            title = activeHome!.name
+            self.tableView.reloadData()
+        } else if homeManager.homes.first != nil {
+            self.activeHome = homeManager.homes.first
+            print("Found first home: \(activeHome), set it to primary")
+            self.homeManager.updatePrimaryHome(activeHome!, completionHandler: { (error) in
+                if error != nil {
+                    print("Something went wrong when attempting to make this home our primary home. \(error?.localizedDescription)")
+                }
+            })
+            self.tableView.reloadData()
+        } else {
+            print("Found no home")
+
+            var alertViewTextField = UITextField()
+            var text = "Default"
+            let alertView = UIAlertController(title: "No home found!", message: "Didn't find any home, please create a new one:", preferredStyle: UIAlertControllerStyle.Alert)
+
+            alertView.addTextFieldWithConfigurationHandler({ (textField) -> Void in
+                alertViewTextField = textField
+            })
+
+            alertView.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: { (alertAction) -> Void in
+                // update the text
+                text = alertViewTextField.text!
+
+                self.homeManager.addHomeWithName(text, completionHandler: { (home, error) in
+                    // what to do here?
+                    if error != nil {
+                        print("Something went wrong when attempting to create our home. \(error?.localizedDescription)")
+                    } else {
+                        print("Successfully added home \(home)")
+                        self.activeHome = home
+                        self.homeManager.updatePrimaryHome(home!, completionHandler: { (error) in
+                            if error != nil {
+                                print("Something went wrong when attempting to make this home our primary home. \(error?.localizedDescription)")
+                            }
+                        })
+                        self.tableView.reloadData()
+                    }
+                })
+                }))
+
+            alertView.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
+
+            dispatch_async(dispatch_get_main_queue(), {
+                self.presentViewController(alertView, animated: true, completion: nil)
+            })
+        }
+    }
 }
